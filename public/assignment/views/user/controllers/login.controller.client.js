01ccada6cb0eca34fdba51af34b6317686b44a4c
chalybes <raphiq@gmail.com>
2017-06-16 00:46:14 -0700

    Added user service

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -3,30 +3,13 @@
         .module('WAM')
         .controller('loginController', loginController);
 
-    function loginController($location) {
+    function loginController($location, userService) {
 
         var model = this;
 
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
-
         model.login = function (username, password) {
-            console.log([username, password]);
 
-            var found = null;
-
-            for (var u in users) {
-
-                var user = users[u];
-
-                if (user.username === username && user.password === password) {
-                    found = user;
-                }
-            }
+            var found = userService.findUserByCredentials(username, password);
 
             if (found !== null) {
                 $location.url('/profile/' + found._id);
--- public/assignment/views/user/controllers/profile.controller.client.js
+++ public/assignment/views/user/controllers/profile.controller.client.js
@@ -3,26 +3,12 @@
         .module('WAM')
         .controller('profileController', profileController);
 
-    function profileController($location, $routeParams) {
+    function profileController($location, $routeParams, userService) {
 
         var model = this;
         var userId = $routeParams['userId'];
 
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
-
-        model.user = findUserById(userId);
-
-        function findUserById(userId) {
-
-            return users.find(function (user) {
-                return user._id === userId;
-            });
-        }
+        model.user = userService.findUserById(userId);
 
     }
 
