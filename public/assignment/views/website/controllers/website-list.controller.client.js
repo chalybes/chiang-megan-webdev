366b694b6a4c1aa880f46db7adffb8a1d4b2b49b
chalybes <raphiq@gmail.com>
2017-06-19 19:44:10 -0700

    Added website service

--- public/assignment/views/website/controllers/website-list.controller.client.js
+++ public/assignment/views/website/controllers/website-list.controller.client.js
@@ -3,34 +3,19 @@
         .module('WAM')
         .controller('websiteListController', websiteListController);
 
-    function websiteListController($routeParams) {
+    function websiteListController($routeParams, websiteService) {
 
         var model = this;
 
         model.userId = $routeParams['userId'];
 
-        var websites = [{ "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
-                        { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
-                        { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
-                        { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
-                        { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-                        { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
-                        { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }];
-
-        model.websites = findAllWebsitesForUser(model.userId);
-
-        function findAllWebsitesForUser(userId) {
-            var resultSet = [];
-
-            for (var w in websites) {
-                if (websites[w].developerId === userId) {
-                    websites[w].created = new Date();
-                    websites[w].updated = new Date();
-                    resultSet.push(websites[w]);
-                }
-            }
-            return resultSet;
+        function init() {
+            model.websites = websiteService.findAllWebsitesForUser(model.userId);
         }
+
+        init();
+
+
 
     }
 
